41c76f3841ed8b83470c60c0350a20c75ca04873
Khoi Bui <kbui8@uncc.edu>
2019-05-25 12:11:15 -0400

    Project structure

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA1jAz42MgUR9DkqeMXKnPNmTAZR6-Ew1w",
+    authDomain: "help-desk-c49dc.firebaseapp.com",
+    databaseURL: "https://help-desk-c49dc.firebaseio.com",
+    projectId: "help-desk-c49dc",
+    storageBucket: "help-desk-c49dc.appspot.com",
+    messagingSenderId: "337548050475",
+    appId: "1:337548050475:web:b037146dafa47d95"
+  }
 };
